4886b77e693014dc7d9fdf5c8d99ff1d5fb578d6
Oscar <43421688+dert98@users.noreply.github.com>
2022-02-18 12:44:48 -0300

    update porcategorias
    improvement php linea 14

--- View/porcategoria.php
+++ View/porcategoria.php
@@ -5,16 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://dert98.github.io/Porfolio/global.css">
 </head>
 <body>
     <div class="container">
         <div class="text-center">
             <?php
+            echo '<label class="bb2 h2 p-2 m-2">';
             if (isset($_GET["idcat"])) {
-                echo '<h2>Categoria '.ucfirst($_GET["categoria"]).'</h2>';
+                echo 'Categoria '.ucfirst($_GET["categoria"]);
             }else{
-                echo '<h2>Todas las Categorias</h2>';
+                echo 'Todas las Categorias';
             }
+            echo "</label>";
             ?>
         </div>
         <div>
@@ -34,8 +37,8 @@
                     $nomimg = $nomimg1.$nomimg2;
                     echo '<div class="card col-md-3">
                         <h5 class="card-header"></h5>
-                        <div class="card-body">
-                            <img src="./Assets/img/'.$nomimg.'.jpg" style="width: 200px; height: 200px;" class="hr1" >
+                        <div class="card-body m-1 p-1">
+                            <img src="./Assets/img/'.$nomimg.'.jpg" style="width: 200px; height: 200px;" class="hr3 hr1" >
                             <h5 class="card-title">'.$pro->nombre.'</h5>
                             <p class="card-text">Materiales:'.$pro->materialidad.' </p>
                             <p class="card-text">Colores:'.$pro->color.' </p>
