03ba371f5081ea5267009a663bcbdd9b3aabc805
Oscar <43421688+dert98@users.noreply.github.com>
2022-02-18 12:17:05 -0300

    filtrado index
    index outside parent  Ok

--- View/porcategoria.php
+++ View/porcategoria.php
@@ -10,34 +10,40 @@
     <div class="container">
         <div class="text-center">
             <?php
-            echo '<h2>Categoria '.ucfirst($_GET["categoria"]).'</h2>';
+            if (isset($_GET["idcat"])) {
+                echo '<h2>Categoria '.ucfirst($_GET["categoria"]).'</h2>';
+            }else{
+                echo '<h2>Todas las Categorias</h2>';
+            }
             ?>
         </div>
         <div>
         <?php
             $dbml = new dbManager("producto","id_producto");
-            $dbml->select();
-            $idb = $_GET["idcat"];
-            $dbml->where('categoria_id','=',$idb);
-            $regs = $dbml->getArray();
-            echo '<div class="container row">';
-            foreach ($regs as $reg) {
-                $pro = new obj($reg);
-                $nomimg1 = "id-".$pro->id_producto;
-                $nomimg2 = "_c-".$pro->categoria_id;
-                $nomimg = $nomimg1.$nomimg2;
-                echo '<div class="card col-md-3">
-                    <h5 class="card-header"></h5>
-                    <div class="card-body">
-                        <img src="./Assets/img/'.$nomimg.'.jpg" style="width: 200px; height: 200px;" >
-                        <h5 class="card-title">Colores: '.$pro->nombre.'</h5>
-                        <p class="card-text">Materiales:'.$pro->materialidad.' </p>
-                        <p class="card-text">Colores:'.$pro->color.' </p>
-                        <a href="View/prod-individual.php?idp='.$pro->id_producto.'" class="btn btn-primary">Comprar</a><label>S/.'.$pro->precio.'</label> 
-                    </div>
-                </div>';
+            if (isset($_GET["idcat"])) {
+                $idb = $_GET["idcat"];
+                $dbml->where('categoria_id','=',$idb);
             }
-            echo '</div>';
+            $dbml->select();
+            $regs = $dbml->getArray();
+                echo '<div class="container row">';
+                foreach ($regs as $reg) {
+                    $pro = new obj($reg);
+                    $nomimg1 = "id-".$pro->id_producto;
+                    $nomimg2 = "_c-".$pro->categoria_id;
+                    $nomimg = $nomimg1.$nomimg2;
+                    echo '<div class="card col-md-3">
+                        <h5 class="card-header"></h5>
+                        <div class="card-body">
+                            <img src="./Assets/img/'.$nomimg.'.jpg" style="width: 200px; height: 200px;" class="hr1" >
+                            <h5 class="card-title">'.$pro->nombre.'</h5>
+                            <p class="card-text">Materiales:'.$pro->materialidad.' </p>
+                            <p class="card-text">Colores:'.$pro->color.' </p>
+                            <a href="View/prod-individual.php?idp='.$pro->id_producto.'" class="btn btn-primary">Comprar</a><label> '.$pro->precio.'</label> 
+                        </div>
+                    </div>';
+                }
+                echo '</div>';
         ?>
         </div>
     </div>
